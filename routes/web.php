471d533d022992932854486f5df34450412ae33b
James <james@gmail.com>
2018-06-23 18:27:06 +0800

    Added project with CORE UI as base template

---
+++ routes/web.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Auth::routes();
+/* CoreUI templates */
+
+Route::middleware('auth')->group(function() {
+	Route::view('/', 'panel.blank');
+	// Section CoreUI elements
+	Route::view('/sample/dashboard','samples.dashboard');
+	Route::view('/sample/buttons','samples.buttons');
+	Route::view('/sample/social','samples.social');
+	Route::view('/sample/cards','samples.cards');
+	Route::view('/sample/forms','samples.forms');
+	Route::view('/sample/modals','samples.modals');
+	Route::view('/sample/switches','samples.switches');
+	Route::view('/sample/tables','samples.tables');
+	Route::view('/sample/tabs','samples.tabs');
+	Route::view('/sample/icons-font-awesome', 'samples.font-awesome-icons');
+	Route::view('/sample/icons-simple-line', 'samples.simple-line-icons');
+	Route::view('/sample/widgets','samples.widgets');
+	Route::view('/sample/charts','samples.charts');
+});
+// Section Pages
+Route::view('/sample/error404','errors.404')->name('error404');
+Route::view('/sample/error500','errors.500')->name('error500');
